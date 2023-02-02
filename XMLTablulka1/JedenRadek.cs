3024e1c38e4645b42a1eea6c562217320b2a8aa4
Martin Csato <mcsato@seznam.cz>
2023-02-03 00:50:59 +0100

    Nová trida jako radek database

---
+++ XMLTablulka1/JedenRadek.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLTabulka1
+{
+    public class JedenRadek
+    {
+        public string MyProperty { get; set; }
+        public string RAZITKO { get; set; }
+        public string SUBJECT1 { get; set; }
+        public string SUBJECT2 { get; set; }
+        public string SUBJECT3 { get; set; }
+        public string SUBJECT4 { get; set; }
+        public string SUBJECT5 { get; set; }
+        public string EXTREF { get; set; }
+        public string CONFIDENT { get; set; }
+        public string rev1 { get; set; }
+        public string rev2 { get; set; }
+        public string rev3 { get; set; }
+        public string rev4 { get; set; }
+        public string rev5 { get; set; }
+        public string date1 { get; set; }
+        public string date2 { get; set; }
+        public string date3 { get; set; }
+        public string date4 { get; set; }
+        public string date5 { get; set; }
+        public string description1 { get; set; }
+        public string description2 { get; set; }
+        public string description3 { get; set; }
+        public string description4 { get; set; }
+        public string description5 { get; set; }
+        public string status1 { get; set; }
+        public string status2 { get; set; }
+        public string status3 { get; set; }
+        public string status4 { get; set; }
+        public string status5 { get; set; }
+        public string name11 { get; set; }
+        public string name12 { get; set; }
+        public string name13 { get; set; }
+        public string name14 { get; set; }
+        public string name15 { get; set; }
+        public string name21 { get; set; }
+        public string name22 { get; set; }
+        public string name23 { get; set; }
+        public string name24 { get; set; }
+        public string name25 { get; set; }
+        public string name31 { get; set; }
+        public string name32 { get; set; }
+        public string name33 { get; set; }
+        public string name34 { get; set; }
+        public string name35 { get; set; }
+        public string name41 { get; set; }
+        public string name42 { get; set; }
+        public string name43 { get; set; }
+        public string name44 { get; set; }
+        public string name45 { get; set; }
+        public string REPLACES { get; set; }
+        public string scale { get; set; }
+        public string sheet { get; set; }
+        public string size { get; set; }
+        public string TYPPROJ { get; set; }
+        public string TASK { get; set; }
+        public string X { get; set; }
+        public string NUMBER { get; set; }
+        public string FOLIO { get; set; }
+        public string REV { get; set; }
+        public string ISSUER { get; set; }
+        public string OCD { get; set; }
+        public string PROJ_NUM { get; set; }
+        public string INDICATOR { get; set; }
+
+    }
+}
